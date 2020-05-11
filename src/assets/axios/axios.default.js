a278a54a923787fce6917117d7631d6f9edead71
1612041_phbinh <1612041_phbinh>
2020-05-12 04:37:40 +0700

    fix bug: localStorage(user) null

--- src/assets/axios/axios.default.js
+++ src/assets/axios/axios.default.js
@@ -2,35 +2,37 @@ import Axios from "axios";
 import { MyStore } from "../..";
 import { history } from "../history/history";
 
-let token = '';
-if (localStorage.getItem('user').token) {
-    token = 'Bearer ' + JSON.stringify(localStorage.getItem('user').token);
-    console.log('token');
+let token = "";
+if (localStorage.getItem("user") && localStorage.getItem("user").token) {
+  token = "Bearer " + JSON.stringify(localStorage.getItem("user").token);
+  console.log("token");
 }
 
 let axios = Axios.create({
-    baseURL: 'http://localhost:8000/',
-    headers: { 'Content-Type': 'application/json', },
-})
+  baseURL: "http://localhost:8000/",
+  headers: { "Content-Type": "application/json" },
+});
 
-axios.interceptors.response.use(response => {
+axios.interceptors.response.use(
+  (response) => {
     return response;
-}, error => {
+  },
+  (error) => {
     console.log(error);
     if (error.response.status === 401) {
-        console.log(error.response.data);
-        alert(error.response.data.message);
+      console.log(error.response.data);
+      alert(error.response.data.message);
 
-        localStorage.setItem('user', null);
-        localStorage.setItem('token', null);
+      localStorage.setItem("user", null);
+      localStorage.setItem("token", null);
 
-        history.push('/login');
-        MyStore.dispatch({
-            type: 'USER_LOG_OUT',
-        })
-     
+      history.push("/login");
+      MyStore.dispatch({
+        type: "USER_LOG_OUT",
+      });
     }
     return Promise.reject(error);
-});
+  }
+);
 
 export default axios;
