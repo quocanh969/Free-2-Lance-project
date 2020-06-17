96e3d63923ee79dcb7667dfc738408730f277598
1612041_phbinh <1612041_phbinh>
2020-06-18 05:17:43 +0700

    show message from change password

--- src/actions/ProfileActions/ChangePassword.js
+++ src/actions/ProfileActions/ChangePassword.js
@@ -1,4 +1,5 @@
 import { changePassword } from "../../services/profile.services";
+import Swal from "sweetalert2";
 
 export const sendchangePassword = (oldPW, newPW) => {
   return (dispatch) => {
@@ -8,41 +9,46 @@ export const sendchangePassword = (oldPW, newPW) => {
         console.log(res);
         if (res.data.code === "-105") {
           // thất bại
-          dispatch(failure(res.data.message));
-          alert(res.data.message);
+          Swal.fire({
+            title: "Đổi mật khẩu không thành công",
+            text: res.data.message,
+            icon: "error",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Ok!",
+          });
         } else {
           // thành công
-          dispatch(success(res.data.message));
-          alert(res.data.message);
+          Swal.fire({
+            title: "Đổi mật khẩu thành công",
+            text: "Vui lòng đăng nhập lại",
+            icon: "success",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "Ok!",
+          }).then((result) => {
+            if (result.value) {
+              localStorage.clear();
+
+              // history.push("/login");
+              window.location.href = "./login";
+            }
+          });
         }
       })
       .catch((err) => {
         console.log(err);
-        dispatch(
-          failure(
-            "There was error with server connection. Error log on console"
-          )
-        );
+        Swal.fire({
+          title: "Đổi mật khẩu không thành công",
+          text: "Đã xảy ra lỗi kết nối",
+          icon: "error",
+          confirmButtonColor: "#3085d6",
+          confirmButtonText: "Ok!",
+        });
       });
   };
 
   function request() {
     return {
       type: "CHANGE_PW_REQUEST",
-    };
-  }
-  function success(message) {
-    console.log("success");
-    return {
-      type: "CHANGE_PW_SUCCESS",
-      message,
-    };
-  }
-  function failure(message) {
-    console.log("failure");
-    return {
-      type: "CHANGE_PW_FAILURE",
-      message,
     };
   }
 };
