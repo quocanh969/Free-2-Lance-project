e72bb0bc34cd0d0f0c9aa44ab4ba70ce2e208295
1612041_phbinh <1612041_phbinh>
2020-07-05 18:26:10 +0700

    spinner for review in dashboard

--- src/components/Profile/Tab/Reviews.js
+++ src/components/Profile/Tab/Reviews.js
@@ -116,7 +116,7 @@ class ReviewsComponent extends Component {
 
     renderReview(reviews) {
         let content = [];
-        if(reviews.length > 0) {
+        if (reviews.length > 0) {
             reviews.forEach((e, index) => {
                 content.push(
                     <li key={index}>
@@ -124,7 +124,7 @@ class ReviewsComponent extends Component {
                             <div className='col-3 profile-img'>
                                 <img src={getImageSrc(null, avatarPlaceholder)} style={{ width: '50px', height: '50px' }}></img>
                             </div>
-    
+
                             <div className='col-9'>
                                 <h4>{e.title}</h4>
                                 <div className='h5'>{e.fullname}</div>
@@ -148,13 +148,13 @@ class ReviewsComponent extends Component {
                     </div>
                 </li>
             )
-        }        
+        }
 
         return content;
     }
 
     render() {
-        let { jobs, totalJob, currentJobPage, tasks, totalTask, currentTaskPage } = this.props.UserDetailReducer;
+        let { jobs, totalJob, currentJobPage, isLoadingJobReview, tasks, totalTask, currentTaskPage, isLoadingTaskReview } = this.props.UserDetailReducer;
         let totalTaskPage = Math.ceil(totalTask / 8);
         let totalJobPage = Math.ceil(totalJob / 8);
 
@@ -176,7 +176,11 @@ class ReviewsComponent extends Component {
                             </div>
                             <div className="content">
                                 <ul className="dashboard-box-list">
-                                    {this.renderReview(tasks)}
+                                    {isLoadingTaskReview ? (<div className="loading" key={1}>
+                                        <div className="spinner-border text-primary" role="status">
+                                            <span className="sr-only">Loading...</span>
+                                        </div>
+                                    </div>) : this.renderReview(tasks)}
                                 </ul>
                             </div>
                         </div>
@@ -192,7 +196,7 @@ class ReviewsComponent extends Component {
                                                 <i className="icon-material-outline-keyboard-arrow-left" />
                                             </div>
                                         </li>
-                                        {this.renderTaskPagination(currentTaskPage, totalTaskPage)}
+                                        {isLoadingTaskReview ? (<div></div>) : this.renderTaskPagination(currentTaskPage, totalTaskPage)}
                                         <li className={"pagination-item " + (totalTaskPage - currentTaskPage < 3 && "d-none")}>
                                             <div className="cursor-pointer page-link" onClick={() => { this.handleTaskPagination(currentTaskPage + 1); }}>
                                                 <i className="icon-material-outline-keyboard-arrow-right" />
@@ -214,7 +218,11 @@ class ReviewsComponent extends Component {
                             </div>
                             <div className="content">
                                 <ul className="dashboard-box-list">
-                                    {this.renderReview(jobs)}
+                                    {isLoadingJobReview ? (<div className="loading" key={1}>
+                                        <div className="spinner-border text-primary" role="status">
+                                            <span className="sr-only">Loading...</span>
+                                        </div>
+                                    </div>) : this.renderReview(jobs)}
                                 </ul>
                             </div>
                         </div>
@@ -230,7 +238,7 @@ class ReviewsComponent extends Component {
                                                 <i className="icon-material-outline-keyboard-arrow-left" />
                                             </div>
                                         </li>
-                                        {this.renderJobPagination(currentJobPage, totalJobPage)}
+                                        {isLoadingJobReview ? (<div></div>) : this.renderJobPagination(currentJobPage, totalJobPage)}
                                         <li className={"pagination-item " + (totalJobPage - currentJobPage < 3 && "d-none")}>
                                             <div className="cursor-pointer page-link" onClick={() => { this.handleJobPagination(currentJobPage + 1); }}>
                                                 <i className="icon-material-outline-keyboard-arrow-right" />
